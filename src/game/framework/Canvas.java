cfd2bf174e1371652a0ee44a9c5245d8ecbf9618
Thomas Skovlund Hansen <thomasskovlundhansen@gmail.com>
2017-03-31 23:36:33 +0200

    Add game and server packages and start defining game interfaces

---
+++ src/game/framework/Canvas.java
@@ -0,0 +1,7 @@
+package game.framework;
+
+/**
+ * Created by fuve on 31/03/2017.
+ */
+public interface Canvas {
+}
---
+++ src/game/framework/Controller.java
@@ -0,0 +1,8 @@
+package game.framework;
+
+/**
+ * Created by fuve on 31/03/2017.
+ */
+public interface Controller {
+    void move(Direction d);
+}
