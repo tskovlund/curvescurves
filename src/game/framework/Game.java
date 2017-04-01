e0a7dc692a2b58635efc1f3581241e247d227134
Jacob <jacobcrawford1@hotmail.com>
2017-04-01 11:30:18 +0200

    Base server is semi integrated with the game, but null padded. (Not functioning)

--- src/game/framework/Game.java
+++ src/game/framework/Game.java
@@ -2,6 +2,8 @@ package game.framework;
 
 import javafx.scene.paint.Color;
 
+import java.util.Map;
+
 /**
  * Created by fuve on 31/03/2017.
  */
@@ -11,4 +13,7 @@ public interface Game {
     void mainLoop();
     void setPlayerDirection(Direction d, Player player);
     void addPlayer(String name, Color color);
+
+    Player getPlayer(String name);
+    Map<Player,Direction> getPlayerMap();
 }
