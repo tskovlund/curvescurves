39bd7d1d8ec3295f222e9a6e072d9bcbe937c513
Thomas Skovlund Hansen <thomasskovlundhansen@gmail.com>
2017-04-02 01:32:15 +0200

    Add connection established toast message

--- js/public/Initialization.js
+++ js/public/Initialization.js
@@ -2,28 +2,28 @@ window.onload = function () {
     let ip = '10.37.0.64';
     let port = '8080';
 
-    socket = new WebSocket('ws://'+ip+':'+port);
-    
+    socket = new WebSocket('ws://' + ip + ':' + port);
+
     // Listen for messages
     socket.addEventListener('message', function (event) {
-	console.log('Message from server', event.data);
+        console.log('Message from server', event.data);
     });
 
     socket.addEventListener('open', function (event) {
-	alert("Connection Established");
-	leftButton = document.getElementById("left");	
-	rightButton = document.getElementById("right");
-	if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-	    setUpListeners('touchend','touchstart');
-	} else {
-	    setUpListeners('mouseup','mousedown');
-	}
+        connectionEstablished();
+        leftButton = document.getElementById("left");
+        rightButton = document.getElementById("right");
+        if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+            setUpListeners('touchend', 'touchstart');
+        } else {
+            setUpListeners('mouseup', 'mousedown');
+        }
     });
 
-    socket.addEventListener('error', function(error) {
-	alert("Connection Failed");
+    socket.addEventListener('error', function (error) {
+        alert("Connection Failed");
     });
-};			   
+};
 
 var socket;
 var leftButton;
@@ -34,45 +34,57 @@ var left = "left";
 var right = "right";
 var forward = "forward";
 
-function setUpListeners(up,down) {    
-    leftButton.addEventListener(down,function(event) {
-	leftDown = true;
+function setUpListeners(up, down) {
+    leftButton.addEventListener(down, function (event) {
+        leftDown = true;
 
-	if (rightDown) {
-	    socket.send(forward);
-	} else {
-	    socket.send(left);
-	}
+        if (rightDown) {
+            socket.send(forward);
+        } else {
+            socket.send(left);
+        }
     });
 
-    rightButton.addEventListener(down,function(event) {
-	rightDown = true;
+    rightButton.addEventListener(down, function (event) {
+        rightDown = true;
 
-	if (leftDown) {
-	    socket.send(forward);
-	} else {
-	    socket.send(right);
-	}
+        if (leftDown) {
+            socket.send(forward);
+        } else {
+            socket.send(right);
+        }
     });
 
-    leftButton.addEventListener(up,function(event) {
-	leftDown = false;
+    leftButton.addEventListener(up, function (event) {
+        leftDown = false;
 
-	if (rightDown) {
-	    socket.send(right);
-	} else {
-	    socket.send(forward);
-	}
+        if (rightDown) {
+            socket.send(right);
+        } else {
+            socket.send(forward);
+        }
     });
 
-    rightButton.addEventListener(up,function(event) {
-	rightDown = false;
+    rightButton.addEventListener(up, function (event) {
+        rightDown = false;
 
-	if (leftDown) {
-	    socket.send(left);
-	} else {
-	    socket.send(forward);
-	}
-    });			   
-			    
+        if (leftDown) {
+            socket.send(left);
+        } else {
+            socket.send(forward);
+        }
+    });
+}
+
+function connectionEstablished() {
+    // Get the snackbar DIV
+    var x = document.getElementById("snackbar")
+
+    // Add the "show" class to DIV
+    x.className = "show";
+
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function () {
+        x.className = x.className.replace("show", "");
+    }, 3000);
 }
