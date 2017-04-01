008e92723464cf892fc0d4da6efda219e6fe3599
Rasmus Skovdal <tokke@tokke.dk>
2017-04-01 10:27:07 +0200

    Added GameConstants

---
+++ src/game/framework/GameConstants.java
@@ -0,0 +1,22 @@
+package game.framework;
+
+import javafx.scene.paint.Color;
+
+/**
+ * Made by Rasmus on 01/04/2017.
+ */
+public class GameConstants {
+    public static final int PLAYER_DOT_DIAMETER = 10;
+    public static final int GAME_WIDTH = 1200;
+    public static final int GAME_HEIGHT = 750;
+    public static final Color BACKGROUND_COLOR = Color.GRAY;
+
+    ////
+    //    SCORE BOARD CONSTANTS
+    ////
+    public static final int scoreOffSetToLeft = 200;
+    public static final int scoreOffSetToTop = 50;
+    public static final int scoreLineHeight = 20;
+    public static final int scoreSpaceForPlayerName = 130;
+
+}
